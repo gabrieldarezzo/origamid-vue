a790b8c4645f54b902585e8cdf56d0ce8432ded8
Darezzo <gabrieldarezzo@yahoo.com.br>
2019-07-03 23:12:19 -0300

    List all products, style them, and get individual

--- src/components/Produtos.vue
+++ src/components/Produtos.vue
@@ -1,10 +1,20 @@
 <template>
-  <section>
-    <div v-for="produto in produtos" :key="produto.id" class="produto">
-      <img :src="produto.img" />
-        {{produto.nome}}
+<div>
+
+  <section class="modal">
+    {{produto}}
+  </section>
+
+  <section class="produtos">
+    <div @click="fetchProduct(item.id)" v-for="item in produtos" :key="item.id" class="produto">
+      <img :src="item.img" :alt="produto.nome" class="produto_img" />
+      <div class="produto_info">
+        <span class="produto_preco">{{item.preco | numberPricePtBr}}</span>
+        <h2 class="produto_titulo">{{item.nome | capitalize}}</h2>
+      </div>
     </div>
   </section>
+</div>
 </template>
 
 <script>
@@ -16,19 +26,37 @@ export default {
   name: 'ApiIextrading',
   data: function () {
     return {
+      produto: {},
       produtos: []
     }
   },
   created () {
     const urlApi = 'http://localhost:3000/produtos'
-
     axios.get(urlApi)
       .then((response) => {
         this.produtos = response.data
       })
   },
   methods: {
-    resultAPI () {}
+    fetchProduct (id) {
+      const urlApi = `http://localhost:3000/produtos/${id}`
+      axios.get(urlApi)
+        .then((response) => {
+          this.produto = response.data
+        })
+    }
+  },
+  filters: {
+    numberPricePtBr (value) {
+      return value.toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+      })
+    },
+    capitalize (value) {
+      if (typeof value !== 'string') return ''
+      return value.charAt(0).toUpperCase() + value.slice(1)
+    }
   }
 
 }
